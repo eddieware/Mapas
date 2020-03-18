6fbf4a85123ac56c5022f7c8e873b8aa451e3573
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-03-18 17:21:00 -0600

    fail_in_callback

--- app/src/main/java/com/example/mapas/MapsActivity.kt
+++ app/src/main/java/com/example/mapas/MapsActivity.kt
@@ -1,7 +1,19 @@
 package com.example.mapas
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,6 +24,17 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    //PERMISOS
+
+    private val permissionFineLocation=android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val permissionCoarseLocation=android.Manifest.permission.ACCESS_COARSE_LOCATION
+
+    //Para identificar el permiso asignado a la ubicacion, puede ir cualquier numero
+    private val CODIGO_SOLICITUD_PERMISO=100
+
+    //variable para obtener latitud y longitud
+    var fusedLocationClient: FusedLocationProviderClient?=null
+    var locationRequest: LocationRequest?=null
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +44,49 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        //inicializamos la variable
+        fusedLocationClient= FusedLocationProviderClient(this)
+        InicializarLocationRequest()
+
+        callback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                super.onLocationResult(p0)
+
+                if (mMap != null) {
+                    mMap.isMyLocationEnabled = true
+                    mMap.uiSettings.isMyLocationButtonEnabled = true
+
+
+                    for (ubicacion in p0?.locations!!) {
+                        //envia mensaje con la latitud y longitud
+                        Toast.makeText(
+                            applicationContext,
+                            ubicacion.latitude.toString() + " , " + ubicacion.longitude.toString(),
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        val miposicion = LatLng(ubicacion.latitude, ubicacion.longitude)
+                        //marcado para texto descriptivo
+                        mMap.addMarker(MarkerOptions().position(miposicion).title("Aqui estoy prros!!"))
+                        //camara
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(miposicion))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun InicializarLocationRequest(){
+        locationRequest= LocationRequest()
+        //intervalo de refresh de la ubicacion en milisegundos
+        locationRequest?.interval=10000
+        //la velocidad mas alta se puede alcanzar para esta misma actualizacion
+        //el intervalopuede variar pero el maximo es cada segundo
+        locationRequest?.fastestInterval=5000
+        //Que tanta proximidad se requiere para que use la localizacion
+        //PRIORITY_HIGH_ACCURACY da la proximidad mas detallada con 10 mets
+        locationRequest?.priority=LocationRequest.PRIORITY_HIGH_ACCURACY
     }
 
     /**
@@ -34,10 +100,56 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.mapType=GoogleMap.MAP_TYPE_HYBRID
 
         // Add a marker in Sydney and move the camera
+        /*
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
     }
+
+    //validacion de permisos o slicitarlos para usar la localizacion
+
+    private fun ValidarPermisosUbicacion():Boolean{
+        //se usa para comparar el permiso de lo que se quiere obtener contra lo que se encuentra en el manifesto
+        //el metodo checkSelfPermision permite comparar un permiso para saber si esta habilitado o denegado
+        val hayubicacionprecisa=
+            ActivityCompat.checkSelfPermission(this,permissionFineLocation)== PackageManager.PERMISSION_GRANTED
+        val hayubicacionordinaria=
+            ActivityCompat.checkSelfPermission(this,permissionCoarseLocation)== PackageManager.PERMISSION_GRANTED
+
+        return hayubicacionprecisa && hayubicacionordinaria
+
+
+    }
+    @SuppressLint("MissingPermission")
+    private fun obtenerubicacion(){
+        //Mnitorea los cambios de ubicacion y los acgtualiza
+        fusedLocationClient?.removeLocationUpdates(locationRequest, callback,null)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun pedirPermisos(){
+        //Esta constante proveerContexto obtiene un balor boleano si ya le pregunto previamente
+        //al usuario si da periso para utilizar su ubicacion
+        //shouldAShowRequestPermissionRationale este metodo permite validar si el usuario cuando
+        //nego permisos sele dio mas informacionde porque son neesarios lospermisos
+
+        val proveerContexto=
+            ActivityCompat.shouldShowRequestPermissionRationale(this,permissionCoarseLocation)
+        if (proveerContexto){
+            solicitudpermiso()
+        }else{
+            solicitudpermiso()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun solicitudpermiso(){
+        //Poner permisos de la actividad en forma de arreglo, junto con el codigo de reconocimiento
+        //desencadena un trigger adicional mediante el metodo OnRequestPermissionResuñt
+        requestPermissions(arrayOf(permissionFineLocation,permissionCoarseLocation),CODIGO_SOLICITUD_PERMISO)
+    }
+
 }
